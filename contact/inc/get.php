5a2f9d4eeac6681d3a7865feab5373410f9befd0
FollowCrom <followcrom@gmail.com>
2023-10-03 15:37:14 +0100

    new box

--- contact/inc/get.php
+++ contact/inc/get.php
@@ -7,26 +7,31 @@
     <p>Let's connect! Drop me a message and I will get back to you within 48 hours.</p>
 
     <div>
-        <h3><label for="name">Name:</label></h3>
-        <input type="text" name="name" id="name" placeholder="Your name (required)"
+        <label for="name">
+            <h3>Name:</h3>
+        </label>
+        <input type="text" name="name" id="name" placeholder="Your name (required)" autocomplete="name"
             value="<?php echo $inputs['name'] ?? '' ?>" class="<?php echo isset($errors['name']) ? 'error' : ''  ?>">
         <div class="errors"><?php echo $errors['name'] ?? '' ?></div>
     </div>
 
     <div>
-        <h3><label for="name">Email:</label></h3>
-        <input type="text" name="email" id="email" placeholder="Your email address (required)"
-            value="<?php echo $inputs['email'] ?? '' ?>" class="<?php echo isset($errors['email']) ? 'error' : '' ?>">
+        <label for="email">
+            <h3>Email:</h3>
+        </label>
+        <input type="email" name="email" id="email" placeholder="Your email (required)" autocomplete="email"
+            value="<?php echo $inputs['email'] ?? '' ?>" class="<?php echo isset($errors['email']) ? 'error' : ''  ?>">
         <div class="errors"><?php echo $errors['email'] ?? '' ?></div>
     </div>
 
     <div>
-        <h3><label for='message'>Message:</label></h3>
+        <label for='message'>
+            <h3>Message:</h3>
+        </label>
         <textarea name="message" id="message" rows="7" cols="37" placeholder="Your message (required)"
-            value="<?php echo $inputs['message'] ?? '' ?>"
             class="<?php echo isset($errors['message']) ? 'error' : '' ?>"><?php echo $inputs['message'] ?? '' ?></textarea>
         <div class="errors"><?php echo $errors['message'] ?? '' ?></div>
     </div>
     <div class="g-recaptcha" data-sitekey="6LdzbkIoAAAAAJxgGPBrMvx688rUFaqoxUD5AMxS"></div>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" aria-label="Submit the form">
 </form>
