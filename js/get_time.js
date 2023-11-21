dcfd988934caf1b7e588e812959e009414ce6632
FollowCrom <followcrom@gmail.com>
2023-11-21 09:42:54 +0000

    Update WotD

--- js/get_time.js
+++ js/get_time.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var greeting = '';
-    var timeOfDay = new Date().getHours();
+document.addEventListener("DOMContentLoaded", function () {
+  var greeting = "";
+  var timeOfDay = new Date().getHours();
 
-    if (timeOfDay >= 6 && timeOfDay < 12) {
-        greeting = 'Good morning';
-    } else if (timeOfDay >= 12 && timeOfDay < 19) {
-        greeting = 'Good afternoon';
-    } else if (timeOfDay >= 19 && timeOfDay < 24) {
-        greeting = 'Good evening';
-    } else {
-        greeting = 'Hi Night Owl';
-    }
+  if (timeOfDay >= 6 && timeOfDay < 12) {
+    greeting = "Good morning";
+  } else if (timeOfDay >= 12 && timeOfDay < 19) {
+    greeting = "Good afternoon";
+  } else if (timeOfDay >= 19 && timeOfDay < 24) {
+    greeting = "Good evening";
+  } else {
+    greeting = "Hi Night Owl";
+  }
 
-    document.getElementById('timeGreeting').textContent = greeting + '. ';
+  document.getElementById("timeGreeting").textContent = greeting;
 });
