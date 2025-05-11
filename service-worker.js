73c01b6af6998dfd3472449c0b7d6f4833c30cdd
followCrom <followcrom@gmail.com>
2025-05-11 23:55:04 +0100

    feat: add service worker for improved caching and offline support

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/main.css',
+          '/images/favs/favicon-32x32.png',
+            '/images/favs/favicon-512x512.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
